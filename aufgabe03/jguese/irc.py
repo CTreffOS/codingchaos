5da3e8f390090beba4ea32cbe7b53c3b65b24775
Justin Güse <jguese@uos.de>
2015-06-10 20:52:12 +0200

    Beauty Work

--- aufgabe03/jguese/irc.py
+++ aufgabe03/jguese/irc.py
@@ -10,28 +10,29 @@ botnick = "GueseBot" # Your bots nick
 def ping(): 
   ircsock.send("PONG :pingis\n")  
 
-def sendmsg(chan , msg):
+def sendmsg(chan , msg): 
   ircsock.send("PRIVMSG "+ chan +" :"+ msg +"\n") 
 
 def joinchan(chan): 
   ircsock.send("JOIN "+ chan +"\n")
 
-def hello(): 
-  ircsock.send("PRIVMSG "+ channel +" :hello!\n")
+def hello():
+  ircsock.send("PRIVMSG "+ channel +" :hello! \o/\n")
                   
 ircsock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 ircsock.connect((server, 6667)) 
-ircsock.send("USER "+ botnick +" "+ botnick +" "+ botnick +"\n") # user authentication
+ircsock.send("USER "+ botnick +" "+ botnick +" "+ botnick +" :Hello everyone!\n")
 ircsock.send("NICK "+ botnick +"\n") 
 
-joinchan(channel) 
+joinchan(channel)
 
 while 1: 
-  ircmsg = ircsock.recv(2048) 
-  print(ircmsg) # Here we print what's coming from the server
+  ircmsg = ircsock.recv(2048)
+  ircmsg = ircmsg.strip('\n\r') 
+  print(ircmsg)
 
-  if ircmsg.find("!say") != -1: #say function
+  if ircmsg.find("!say") != -1: 
     hello()
 
-  if ircmsg.find("PING :") != -1: # if the server pings us then we've got to respond!
+  if ircmsg.find("PING :") != -1: 
     ping()
