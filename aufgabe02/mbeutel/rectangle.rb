745aaf7613380308e7feef6975934a24c36d6bab
MiriamBeutel <miriam.beutel@gmx.de>
2015-06-10 19:43:04 +0200

    lilchange

--- aufgabe02/mbeutel/rectangle.rb
+++ aufgabe02/mbeutel/rectangle.rb
@@ -1,7 +1,5 @@
-width = ARGV[0].to_i
-height = ARGV[1].to_i
+width, height = ARGV[0].to_i, ARGV[1].to_i
 numbers = []
-
 while width > 0 && height > 0
 	if width >= height
 		numbers << height
@@ -9,8 +7,7 @@ while width > 0 && height > 0
 	else
 		numbers << width
 		height -= width
-	end
-	
+	end	
 end
 print numbers
 puts ""
