7559358651efc4c1c679a862d287086a65432b8d
funnyflowerpot <oberwipf@gmail.com>
2015-06-11 09:46:09 +0200

    unnuetze abhaengigkeit entfernt

--- aufgabe05/funnyflowerpot/aufgabe5.py
+++ aufgabe05/funnyflowerpot/aufgabe5.py
@@ -1,3 +1,3 @@
-import sys,math,itertools
+import sys,math
 r,s,t=map(float,sys.argv[1:])
 print filter(lambda(n,t):t,enumerate(map(lambda o,a:o(a),[any,all,any,any],zip(*map(lambda w:(w==0,w<1 and w!=0,w==1,w>1),map(lambda a,b,c:a*b>0 and(a**2+b**2-c**2)/(2*a*b)or 0,[r,s,t],[s,r,t],[t,r,s]))))))[0][0]
