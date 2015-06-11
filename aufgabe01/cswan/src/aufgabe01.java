65d70f2880d57e6bcf90eb0acc0aa8ffe26c0522
Jochen Saalfeld <git@jochen-saalfeld.de>
2015-06-11 11:58:53 +0200

    cleaning merge conflict

--- aufgabe01/cswan/src/aufgabe01.java
+++ aufgabe01/cswan/src/aufgabe01.java
@@ -1,16 +1,6 @@
 public class aufgabe01 {
     public static void main(String[] args) {
         int num = Integer.parseInt(args[0]);
-
-<<<<<<< HEAD
-        if(num % 3 == 0)
-            System.out.print("Fizz");
-
-        if(num % 5 == 0)
-            System.out.println("Buzz");
-
-        System.out.println("\n");
-=======
         for(int i = 1; i <= num; i++) {
             System.out.print(i + ": ");
 
@@ -22,6 +12,5 @@ public class aufgabe01 {
 
             System.out.println();
         }
->>>>>>> 7820a91e8a75c754a12fc46b5ac0c692b2267a8f
     }
 }
